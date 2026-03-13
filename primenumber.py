c39c70c9af287f04d554dae00f32d62762cd9c96
marvilopd <marvinschuz14@gmail.com>
2026-03-13 18:22:50 +0100

    Delete primenumber.py

--- primenumber.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a number: "))
-for i in range(2, n):
-    if n % i == 0:
-        print(f"{n} is not a prime number") 
-        break
-if i == n - 1:
-    print(f"{n} is a prime number")
